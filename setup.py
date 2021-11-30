d89eb9747d10eb5753d39a5c7c005d14c211b1d8
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-11-30 16:42:33 -0600

    Set dev status

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setuptools.setup(
     download_url='https://github.com/BobDotCom/mclauncher/releases',
     packages=packages,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
