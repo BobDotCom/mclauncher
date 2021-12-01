d93fb4dd206942ff2c2d9f1ed2bbcdb87b1d84f8
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-12-01 10:22:57 -0600

    Bump version: 0.1.4 → 0.1.5

--- mclauncher/main.py
+++ mclauncher/main.py
@@ -39,7 +39,7 @@ from .pipe import LogPipe
 from .webserver import run_server
 from .ui import Gui, Cli
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 def launch(gui: bool = False, args=None):
